b2cf12d784cd6ebb61c06a7fd1444f193870dc85
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-02-27 14:23:16 +0530

    setup express server

--- server.js
+++ server.js
@@ -1,43 +1,13 @@
-const http = require('http');
-const todos = [
-    {
-        id: 1,
-        name: 'todo 1',
-    },
-    {
-        id: 2,
-        name: 'todo 2',
-    },
-];
-const server = http.createServer((req, res) => {
-    console.log('authorization', req.headers.authorization);
-    let body = [];
-    let response = {
-        success: false,
-        data: null,
-    };
-    let statusCode = 404;
-    const header = {
-        'Content-Type': 'application/json',
-    };
+const express = require('express');
+const dotenv = require('dotenv');
 
-    res.writeHead(statusCode, header);
-    req.on('data', (data) => {
-        body.push(data);
-    }).on('end', () => {
-        const str = Buffer.concat(body).toString();
-        console.log('req.method', req.method);
-        console.log('req.url', req.url);
-        if (req.method === 'GET' && req.url === '/todos') {
-            statusCode = 200;
-            response.success = true;
-            response.data = todos;
-        }
-        res.writeHead(statusCode, header);
-        res.end(JSON.stringify(response));
-    });
-});
+dotenv.config();
 
-const PORT = 5000;
+const app = express();
 
-server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
